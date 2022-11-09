df9eba8ccbebca12d2dbfb94887c2104b7ca1d3d
sonimohit481 <sonimohit481@gmail.com>
2022-11-09 18:13:40 +0530

    first commit React installed and a rough sketch is done

---
+++ src/Compoments/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+import { HiMailOpen } from "@react-icons/all-files/hi/HiMailOpen";
+export const Navbar = () => {
+  return (
+    <div id="nav">
+      <HiMailOpen style={{ color: "purple" }} />
+    </div>
+  );
+};
