e6e3c69c24b41c8f86f1200477acfe80fd7825c9
sonimohit481 <sonimohit481@gmail.com>
2022-11-10 09:27:23 +0530

    few test changes

--- src/Compoments/Navbar.jsx
+++ src/Compoments/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import "./Navbar.css";
 import { HiMailOpen } from "@react-icons/all-files/hi/HiMailOpen";
+import { BiChevronDown } from "@react-icons/all-files/bi/BiChevronDown";
 export const Navbar = () => {
   return (
-    <div id="nav">
-      <HiMailOpen style={{ color: "purple" }} />
+    <div id="Nav">
+      <div id="Subnav1">
+        <div>
+          <HiMailOpen style={{ color: "6E66E6", fontSize: "32px" }} />
+        </div>
+        <div>
+          <h3>Estatery</h3>
+        </div>
+      </div>
+      <div id="Subnav2">
+        <ul>
+          <li>Rent</li>
+          <li>Buy</li>
+          <li>Sell</li>
+          <li>
+            Manage Property <BiChevronDown />{" "}
+          </li>
+          <li>
+            Resources <BiChevronDown />
+          </li>
+        </ul>
+      </div>
+      <div id="Subnav3">
+        <button>Login</button>
+        <button>Sign in</button>
+      </div>
     </div>
   );
 };
